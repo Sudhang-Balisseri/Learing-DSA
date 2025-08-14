72742086875a92ac7eafc831ec80fd05c3be8211
Sudhang-Balisseri <balisseri@gmail.com>
2025-08-14 18:27:27 +0530

    some-questions-of-array

---
+++ C++/Array/Ques/code.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    // ***************************************
+
+    // Q1
+    // int marks[5];
+    // int size = 5;
+
+    // for (int i=0; i<size; i++)
+    // {
+    //     cin >> marks[i];
+    // }
+
+    // for (int i=0; i<size; i++)
+    // {
+    //     cout << marks[i] << " ";
+    // }
+    // cout << endl;
+
+    // return 0;
+
+    // ****************************************
+
+     // Q2
+    // int marks[6] = {15, 3, 55, -15, 24, 92};
+    // int size = 6;
+    // int smallest = INT_MAX;
+    
+    // for (int i=0; i<size; i++)
+    // {
+    //     if (marks[i] < smallest)
+    //     {
+    //         smallest = marks[i];
+    //     }
+    // }
+
+    // cout << smallest << endl;
+    // return 0;
+
+    // ******************************************
+
+    // Q3
+    // int marks[6] = {15, 3, 55, -15, 24, 92};
+    // int size = 6;
+    // int largest = INT_MIN;
+    
+    // for (int i=0; i<size; i++)
+    // {
+    //     largest = max(marks[i], largest);
+    // }
+
+    // cout << largest << endl;
+    // return 0;
+
+    // ******************************************
+
+    //Q4
+    // int marks[6] = {15, 3, 55, -15, 24, 92};
+    // int size = 6;
+    // int smallest = INT_MAX;
+    // int largest = INT_MIN;
+    // int idxSmallest;
+    // int idxLargest;
+    
+    // for (int i=0; i<size; i++)
+    // {
+    //     if (marks[i] < smallest)
+    //     {
+    //         smallest = marks[i];
+    //         idxSmallest = i;
+    //     }
+    //     if (marks[i] > largest)
+    //     {
+    //         largest = marks[i];
+    //         idxLargest = i;
+    //     }
+        
+    // }
+
+
+    // cout << smallest << endl;
+    // cout << idxSmallest << endl;
+    // cout << largest << endl;
+    // cout << idxLargest << endl;
+    // return 0;
+}
