a4da56b49c5bc142fa9769b1596126eeb7369f0e
Sudhang-Balisseri <balisseri@gmail.com>
2025-09-05 18:05:03 +0530

    basic-of-array

--- C++/Array/Ques/code.cpp
+++ C++/Array/Ques/code.cpp
@@ -2,41 +2,74 @@
 #include <climits>
 using namespace std;
 
+void reverseArray (int arr[], int sz)
+{
+    int start = 0 , end = sz - 1;
+
+    while (start < end)
+    {
+        swap(arr[start], arr[end]);
+        start ++;
+        end --;
+    }
+    
+}
+
+
 int main()
 {
-    int num[] = {11, 15, 69, 12,  2, -100};
-    int size = 6; 
-    int smallest = INT_MAX;
-    int largest = INT_MIN;
-    int indxOfSmallest;
-    int indxOfLargest;
+    //Q1
+
+    // int num[] = {11, 15, 69, 12,  2, -100};
+    // int size = 6; 
+    // int smallest = INT_MAX;
+    // int largest = INT_MIN;
+    // int indxOfSmallest;
+    // int indxOfLargest;
 
 
-    for (int i=0; i<size; i++)
+    // for (int i=0; i<size; i++)
+    // {
+    //     // 1 way
+    //     if (num[i] < smallest)
+    //     {
+    //         smallest = num[i];
+    //         indxOfSmallest = i;
+    //     }
+
+    //     if (num[i] > largest)
+    //     {
+    //         largest = num[i];
+    //         indxOfLargest = i;
+    //     }
+
+    //     // 2 way
+    //     smallest = min(num[i], smallest);
+    //     largest = max(num[i], largest);
+
+    // }
+
+    // cout << "the smallest num --> " << smallest << endl;
+    // cout << "and the indx of smallest --> " << indxOfSmallest << endl;
+    // cout << "the largest num --> " << largest << endl;
+    // cout << "and the index of largest num --> " << indxOfLargest << endl;
+
+    // return 0;
+
+    // ********************************************************************
+
+    //Q2
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int sz = 7;
+
+    reverseArray(arr, sz);
+
+    for (int i=0; i<sz; i++)
     {
-        // 1 way
-        if (num[i] < smallest)
-        {
-            smallest = num[i];
-            indxOfSmallest = i;
-        }
-
-        if (num[i] > largest)
-        {
-            largest = num[i];
-            indxOfLargest = i;
-        }
-
-        // 2 way
-        smallest = min(num[i], smallest);
-        largest = max(num[i], largest);
-
+        cout << arr[i] << " ";
     }
 
-    cout << "the smallest num --> " << smallest << endl;
-    cout << "and the indx of smallest --> " << indxOfSmallest << endl;
-    cout << "the largest num --> " << largest << endl;
-    cout << "and the index of largest num --> " << indxOfLargest << endl;
-
+    cout << endl;
     return 0;
 }
--- C++/Array/basic/code.cpp
+++ C++/Array/basic/code.cpp
@@ -1,36 +1,86 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+
+void changeArr(int arr[], int size)
 {
-    int marks[5] = {99, 100, 54, 36, 88};
-    
-    
-    marks[0] = 101; // Array Index
-
-    cout << marks[0] << endl;
-    cout << marks[1] << endl;
-    cout << marks[2] << endl;
-    cout << marks[3] << endl;
-    cout << marks[4] << endl;
-
-    cout << endl;
-
-    // loops on Array
-    int size = 5;
-    int marks2[size] = {};
-    
-    for(int i=0; i<size; i++)
-    {
-        cin >> marks2[i];
-    }
-    
+    cout << "in function \n";
     for (int i=0; i<size; i++)
     {
-        cout << marks2[i] << endl;
+        arr[i] = 2* arr[i];
     }
- 
-    double price[] = {99.9, 88.9, 99.3};
+}
 
+int linearSearch(int arr2[], int sz, int target)
+{
+    for(int i=0; i<sz; i++)
+    {   
+        if (arr2[i] == target)
+        {
+            return i; // FOUND
+        }
+    }
+    return -1; //NOT FOUND
+}
+
+int main ()
+{
+    // int marks[5] = {99, 100, 54, 36, 88};
+    
+    
+    // marks[0] = 101; // Array Index
+
+    // cout << marks[0] << endl;
+    // cout << marks[1] << endl;
+    // cout << marks[2] << endl;
+    // cout << marks[3] << endl;
+    // cout << marks[4] << endl;
+
+    // cout << endl;
+
+    // loops on Array
+    // int size = 5;
+    // int marks2[size] = {};
+    
+    // for(int i=0; i<size; i++)
+    // {
+    //     cin >> marks2[i];
+    // }
+    
+    // for (int i=0; i<size; i++)
+    // {
+    //     cout << marks2[i] << endl;
+    // }
+ 
+    // double price[] = {99.9, 88.9, 99.3};
+
+// *******************************************************
+
+    // cout << endl;
+
+// **********************************************************
+
+//     //Pass By Reference
+    // int arr[] = {1, 2, 3};
+    
+    
+    // changeArr(arr, 3);
+
+    // cout << "in main \n";
+    // for (int i=0; i<3 ; i++)
+    // {
+    //     cout << arr[i] << endl;
+    // }
+    // return 0;
+
+// // ******************************************************
+    // cout << endl;
+//  ****************************************************   
+    //Linear Search
+    int arr2[] = {4, 2, 7, 8, 1, 2, 5};
+    int sz = 7;
+    int target = 8;
+
+    cout << linearSearch(arr2, sz, target) << endl;
     return 0;
 }
